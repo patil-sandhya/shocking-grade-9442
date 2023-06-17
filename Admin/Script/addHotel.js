7779c058fd21485fd0339207303486421a1e43a7
PatilSandhya <begadesandhya@gmail.com>
2023-06-17 19:50:03 +0530

    day-5

--- Admin/Script/addHotel.js
+++ Admin/Script/addHotel.js
@@ -1,20 +1,25 @@
 let myform = document.getElementById("myform")
 myform.addEventListener("submit", postData)
 
+let url = `https://hid-food-apii.onrender.com/product_data`
 
 async function postData(){
     event.preventDefault();
     
     try {
-        let res = await fetch("http://localhost:3000/blogs",
+        let res = await fetch(url,
     {
       method:"POST",
       body: JSON.stringify(
         {
-            "title": myform.title.value,
-            "img": myform.blgImg.value,
-            "description": myform.description.value,
-            "date":myform.date.value
+          "name": myform.title.value,
+          "avatar": myform.hotelImg.value,
+          "details": myform.description.value,
+          "distance":myform.distance.value,
+          "price": myform.price.value,
+          "tax": myform.tax.value,
+          "review":"",
+          "rating":""
         }
       ),
       headers:{
